c60556ce1f4407e1249bdfebd7c0dfd850a1d91f
Danil Chesnokov <shaytann831@gmail.com>
2024-08-17 11:35:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let s;
+    if (n >= 0) {
+        s = n.toString();
+    } else {
+        s = (-n).toString();
+    }
+    let res = '';
+    for (let i = s.length - 1; i >= 0; i--){
+      res += s[i];
+    }
+    return Number(res);
 }
+
+
